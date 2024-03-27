c763fd241feb5b7c2a9fae1e2b9093e9844ecb0a
Đàn <96944395+dan-tran-van@users.noreply.github.com>
2024-03-27 23:40:25 +0700

    Update settings.py for first deployment
    Add ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS
    settings

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -38,8 +38,17 @@ SECRET_KEY = os.environ.get(
 # DEBUG = True
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dantran.pythonanywhere.com', '127.0.0.1']
 
+# During development, you can instead set just the base URL
+# (you might decide to change the site a few times).
+# ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
+
+
+CSRF_TRUSTED_ORIGINS = ['https://dantran.pythonanywhere.com']
+# During development/for this tutorial you can instead set just the base URL
+# CSRF_TRUSTED_ORIGINS = ['https://*.pythonanywhere.com']
 
 # Application definition
 
